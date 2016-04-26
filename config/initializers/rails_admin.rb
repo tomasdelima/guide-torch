b409798bd649bff9557d24bea7c4fc48ea672fd1
Tomás de Lima <tomasdelima@gmail.com>
2016-04-26 01:08:00 -0300

    Configures RailsAdmin

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,23 +1,38 @@
 RailsAdmin.config do |config|
+  config.authenticate_with do
+    warden.authenticate! scope: :user
+  end
+  config.current_user_method(&:current_user)
 
-  ### Popular gems integration
+  ### Models ###
 
-  ## == Devise ==
-  # config.authenticate_with do
-  #   warden.authenticate! scope: :user
-  # end
-  # config.current_user_method(&:current_user)
+  config.model 'User' do
+    navigation_label 'Configurações'
+    label 'Usuário'
+    exclude_fields :_id, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :sign_in_count, :current_sign_in_at, :last_sign_in_at, :current_sign_in_ip, :last_sign_in_ip
+  end
 
-  ## == Cancan ==
-  # config.authorize_with :cancan
+  config.model 'Document' do
+    navigation_label 'Configurações'
+    label 'Documento'
+    exclude_fields :_id
+  end
 
-  ## == Pundit ==
-  # config.authorize_with :pundit
+  config.model 'Author' do
+    navigation_label 'Configurações'
+    label 'Autor'
+    label_plural 'Autores'
+    exclude_fields :_id
+  end
 
-  ## == PaperTrail ==
-  # config.audit_with :paper_trail, 'User', 'PaperTrail::Version' # PaperTrail >= 3.0.0
+  config.model 'Category' do
+    navigation_label 'Configurações'
+    label 'Categoria'
+    label_plural 'Categorias'
+    exclude_fields :_id
+  end
 
-  ### More at https://github.com/sferik/rails_admin/wiki/Base-configuration
+  ### Controllers ###
 
   config.actions do
     dashboard                     # mandatory
